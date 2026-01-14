6e1999abcdbf951770624a1b4cd1cefdce78fdd3
growthsoftwaresolutions-a11y <growthsoftwaresolutions@gmail.com>
2026-01-14 22:34:35 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
-import contactRoutes from "./routes/contactRoutes.js";
+import contactRoutes from "./contactRoutes.js";
 
 dotenv.config();
 
